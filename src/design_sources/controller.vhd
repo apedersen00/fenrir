043d6e8f5233ca75b5528876a924027bf1904a43
alexthch <201506019@post.au.dk>
2025-02-26 10:42:01 +0100

    bugfixes for synthesis

--- src/design_sources/controller.vhd
+++ src/design_sources/controller.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity controller is
     port (
@@ -9,7 +8,6 @@ entity controller is
         rst             : in std_logic;
         neuron_address  : out std_logic_vector(7 downto 0);
         synapse_address : out std_logic_vector(15 downto 0);
-
         input_in        : in std_logic_vector(7 downto 0);
         input_out       : out std_logic
     );
@@ -17,43 +15,45 @@ end controller;
 
 architecture Behavioral of controller is
 
-    signal counter_neuron   : std_logic_vector(7 downto 0)  := (others => '0');
-    signal counter_synapse  : std_logic_vector(7 downto 0)  := (others => '0');
-    signal counter_input    : std_logic_vector(7 downto 0)  := (others => '0');
-
-    variable last_neuron    : std_logic_vector(7 downto 0)  := "00110000"; --dec: 48 -- 
-    variable last_synapse   : std_logic_vector(7 downto 0)  := "00110000";
+    signal counter_neuron  : unsigned(7 downto 0) := (others => '0');
+    signal counter_synapse : unsigned(7 downto 0) := (others => '0');
+    signal counter_input   : unsigned(7 downto 0) := (others => '0');
 
 begin
     process (clk)
+        constant last_neuron  : unsigned(7 downto 0) := to_unsigned(48, 8);
+        constant last_synapse : unsigned(7 downto 0) := to_unsigned(48, 8);
     begin
         if rising_edge(clk) then
-
             if rst = '1' then
-
-                counter_neuron <= (others => '0');
+                counter_neuron  <= (others => '0');
                 counter_synapse <= (others => '0');
-                counter_input <= (others => '0');
+                counter_input   <= (others => '0');
+                neuron_address  <= (others => '0');
+                synapse_address <= (others => '0');
+                input_out       <= '0';
 
             elsif counter_neuron = last_neuron then
-
-                counter_neuron <= (others => '0');
+                counter_neuron  <= (others => '0');
                 counter_synapse <= (others => '0');
-                counter_input <= (others => '0');
+                counter_input   <= (others => '0');
 
             elsif counter_synapse = last_synapse then
-
-                counter_neuron <= counter_neuron + 1;
+                counter_neuron  <= counter_neuron + 1;
                 counter_synapse <= (others => '0');
-                
-            else 
 
-                neuron_address <= counter_neuron;
-                synapse_address(7 downto 0) <= counter_synapse;
-                synapse_address(15 downto 8) <= counter_neuron;
-                input_out <= input_in(counter_input);
-                counter_synapse <= counter_synapse + 1;
+            else 
+                neuron_address  <= std_logic_vector(counter_neuron);
+                synapse_address(7 downto 0) <= std_logic_vector(counter_synapse);
+                synapse_address(15 downto 8) <= std_logic_vector(counter_neuron);
                 
+                if to_integer(counter_input) < 8 then
+                    input_out <= input_in(to_integer(counter_input));
+                else
+                    input_out <= '0';
+                end if;
+
+                counter_synapse <= counter_synapse + 1;
             end if;
         end if;
     end process;
