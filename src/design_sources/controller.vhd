511191cee2fb102f2104055171138695220e565f
alexthch <201506019@post.au.dk>
2025-02-27 19:35:54 +0100

    formatting & extra business logic for w-ops

--- src/design_sources/controller.vhd
+++ src/design_sources/controller.vhd
@@ -16,6 +16,8 @@ entity controller is
         -- neuron and synapse 
         neuron_address  : out std_logic_vector(7 downto 0); -- 8-bit address for neuron
         neuron_input    : in std_logic_vector(31 downto 0); -- 32-bit input for neuron
+        neuron_we       : out std_logic; -- write enable for neuron
+        neuron_update   : out std_logic_vector(31 downto 0); -- 32-bit update for neuron
 
         synapse_address : out std_logic_vector(7 downto 0); -- 8-bit address for synapse
         synapse_in      : in std_logic_vector(31 downto 0); -- 32-bit input for synapse
@@ -62,16 +64,21 @@ begin
         
     begin
         if rising_edge(clk) then
+
             case MAIN_STATE is
+
                 when IDLE =>
-                    -- waiting for data
+                    
                     if data_rdy = '1' then
                         -- data is ready, flip the ready signal and start getting neuron and synapse
-                        ready <= '0';
-                        MAIN_STATE <= READ_REQUEST;
-                    end if;
+                        ready       <= '0';
+                        MAIN_STATE  <= READ_REQUEST;
 
-                    ready <= '1';
+                    else 
+
+                        ready       <= '1';
+
+                    end if;
 
                 when READ_REQUEST => 
 
@@ -84,37 +91,48 @@ begin
 
                             -- read neuron
                             -- convert neuron counter to 8-bit address
-                            neuron_address <= std_logic_vector(to_unsigned(neuron_counter, 8));
-                            MAIN_STATE <= UPDATE;
+                            neuron_we       <= '0';
+                            neuron_address  <= std_logic_vector(to_unsigned(neuron_counter, 8));
+                            MAIN_STATE      <= UPDATE;
 
                         when READ_SYNAPSE =>
 
                             -- read synapse
                             if (synapse_counter > 15 and synapse_counter < 32) then
+
                                 -- multiply neuron counter by 3 and add 1
                                 synapse_address <= std_logic_vector(to_unsigned(neuron_counter * 3 + 1, 8));
+
                             else
+
                                 -- multiply neuron counter by 3 and add 2
                                 synapse_address <= std_logic_vector(to_unsigned(neuron_counter * 3 + 2, 8));
+
                             end if;
 
-                            MAIN_STATE <= COMPUTE;
+                            MAIN_STATE      <= COMPUTE;
 
                         when READ_BOTH =>
 
                             -- read both
                             -- multiply neuron counter by 3
-                            synapse_address <= std_logic_vector(to_unsigned(neuron_counter * 3, 8));
+                            synapse_address     <= std_logic_vector(to_unsigned(neuron_counter * 3, 8));
                             -- same address for neuron
-                            neuron_address <= std_logic_vector(to_unsigned(neuron_counter, 8));
+                            neuron_address      <= std_logic_vector(to_unsigned(neuron_counter, 8));
 
                     end case;
 
-                    
-
                 when WRITE_REQUEST =>
+
                     -- for updating neuron
+                    neuron_we       <= '1';
+                    neuron_update   <= temp_state_core;
+
+                    MAIN_STATE      <= READ_REQUEST;
+                    REQUEST_STATE   <= READ_BOTH;
+
                 when UPDATE => 
+
                     -- update internal signals and variables 
                     temp_state_core := neuron_input(11 downto 0);
 
@@ -122,46 +140,51 @@ begin
                     -- main loop
                     -- start updating the neuron signals from the 32 bit word from neuron bram (neuron_input)
 
-                    param_leak_str <= neuron_input(30 downto 24);
-                    param_thr <= neuron_input(23 downto 12);
-                    state_core <= temp_state_core;
+                    param_leak_str      <= neuron_input(30 downto 24);
+                    param_thr           <= neuron_input(23 downto 12);
+                    state_core          <= temp_state_core;
+
                     -- we must update the synapse weight. Since we are reading synapses as 32 bit words, we need to extract the weight from the word
                     -- each synapse_in word contains 16 weights. We use the counter signal to index the 2 bit weight
-                    syn_weight <= synapse_in(2 * synapse_counter + 1 downto 2 * synapse_counter);
+                    syn_weight          <= synapse_in(2 * synapse_counter + 1 downto 2 * synapse_counter);
 
                     -- pass the input bit from the input vector. Indexed by the synapse counter
-                    syn_event <= input_vector(synapse_counter - (input_row_counter * 16));
+                    syn_event           <= input_vector(synapse_counter - (input_row_counter * 16));
 
                     -- increment the synapse counter
-                    synapse_counter <= synapse_counter + 1;
+                    synapse_counter     <= synapse_counter + 1;
 
                     -- read the state core from the state_core_next signal
-                    temp_state_core := state_core_next;
+                    temp_state_core     := state_core_next;
 
                     -- check if the neuron has spiked
                     if spike_out = '1' then
-                        -- if the neuron has spiked we want to do something. Maybe just go to next neuron and store a spike somewhere
+
+                        -- if the neuron has spiked we want to do something. 
+                        -- Maybe just go to next neuron and store a spike somewhere ? todo!!!
                         synapse_counter <= 48;
 
                     end if;
 
                     -- check synapse counter
                     if synapse_counter = 16 or synapse_counter = 32 then
+
                         -- we need to read the following 16 weights and increment the row counter. 
-                        MAIN_STATE <= READ_REQUEST;
-                        REQUEST_STATE <= READ_SYNAPSE;
+                        MAIN_STATE      <= READ_REQUEST;
+                        REQUEST_STATE   <= READ_SYNAPSE;
                         
                     elsif synapse_counter = 48 then 
+
                         -- next neuron
                         synapse_counter <= 0;
-                        neuron_counter <= neuron_counter + 1;
-                        MAIN_STATE <= READ_REQUEST;
-                        REQUEST_STATE <= READ_BOTH;
-
+                        neuron_counter  <= neuron_counter + 1;
+                        MAIN_STATE      <= WRITE_REQUEST;
+        
                     end if;
 
                 when others => 
-                    MAIN_STATE <= IDLE;
+                
+                    MAIN_STATE          <= IDLE;
 
             end case;
         end if;
