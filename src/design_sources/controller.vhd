a751452765ea58c1cd41f9842d4a59fe357c8d87
alexthch <201506019@post.au.dk>
2025-02-27 15:50:36 +0100

    working on main loop

--- src/design_sources/controller.vhd
+++ src/design_sources/controller.vhd
@@ -6,6 +6,8 @@ entity controller is
     port (
 
         clk             : in std_logic;
+        ready           : out std_logic; -- ready signal for data
+
         -- Data
         input_vector    : in std_logic_vector(15 downto 0); -- 16-bit input
         input_select    : out std_logic_vector(3 downto 0); -- 4-bit selector for input
@@ -22,7 +24,7 @@ entity controller is
         param_leak_str  : out std_logic_vector(6 downto 0); -- leakage stength parameter
         param_thr       : out std_logic_vector(11 downto 0); -- neuron firing threshold parameter
         state_core      : out std_logic_vector(11 downto 0); -- core neuron state from SRAM
-        syn_weight      : out std_logic_vector(3 downto 0); -- synaptic weight
+        syn_weight      : out std_logic_vector(1 downto 0); -- synaptic weight
         syn_event       : out std_logic; -- synaptic event trigger
         
         state_core_next : in std_logic_vector(11 downto 0); -- next core neuron state to SRAM
@@ -32,8 +34,9 @@ entity controller is
 end controller;
 
 architecture Behavioral of controller is
-    signal synapse_counter  : integer := 0;
-    signal neuron_counter   : integer := 0;
+    signal synapse_counter          : integer := 0;
+    signal neuron_counter           : integer := 0;
+    signal input_row_counter        : integer := 0;
 
     type main_states is (
         IDLE,
@@ -47,19 +50,22 @@ architecture Behavioral of controller is
         READ_BOTH
     );
 
-    signal MAIN_STATE : main_states := IDLE;
-    signal REQUEST_STATE : read_request_substates := READ_NEURON;
+    signal MAIN_STATE               : main_states := IDLE;
+    signal REQUEST_STATE            : read_request_substates := READ_BOTH;
     
 begin
 
     process (clk)
 
+        
     begin
         if rising_edge(clk) then
             case MAIN_STATE is
                 when IDLE =>
                     -- waiting for data
                     if data_rdy = '1' then
+                        -- data is ready, flip the ready signal and start getting neuron and synapse
+                        ready <= '1';
                         MAIN_STATE <= READ_REQUEST;
                     end if;
 
@@ -98,6 +104,18 @@ begin
 
                 when COMPUTING => 
                     -- main loop
+                    -- start updating the neuron signals from the 32 bit word from neuron bram (neuron_input)
+
+                    param_leak_str <= neuron_input(30 downto 24);
+                    param_thr <= neuron_input(23 downto 12);
+                    state_core <= neuron_input(11 downto 0);
+
+                    -- we must update the synapse weight. Since we are reading synapses as 32 bit words, we need to extract the weight from the word
+                    -- each synapse_in word contains 16 weights. We use the counter signal to index the 2 bit weight
+                    syn_weight <= synapse_in(2 * synapse_counter + 1 downto 2 * synapse_counter);
+
+                    -- pass the input bit from the input vector. Indexed by the synapse counter
+
 
                 when others => 
                     MAIN_STATE <= IDLE;
