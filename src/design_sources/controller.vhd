481ffec1bd131b8d9b3a0977fa0fef8783013118
alexthch <201506019@post.au.dk>
2025-02-27 16:11:27 +0100

    almost all business logic
    i think?

--- src/design_sources/controller.vhd
+++ src/design_sources/controller.vhd
@@ -42,7 +42,8 @@ architecture Behavioral of controller is
         IDLE,
         READ_REQUEST,
         WRITE_REQUEST,
-        COMPUTING
+        UPDATE,
+        COMPUTE
     );
     type read_request_substates is (
         READ_NEURON,
@@ -57,6 +58,7 @@ begin
 
     process (clk)
 
+        variable temp_state_core : std_logic_vector(11 downto 0);
         
     begin
         if rising_edge(clk) then
@@ -65,20 +67,28 @@ begin
                     -- waiting for data
                     if data_rdy = '1' then
                         -- data is ready, flip the ready signal and start getting neuron and synapse
-                        ready <= '1';
+                        ready <= '0';
                         MAIN_STATE <= READ_REQUEST;
                     end if;
 
+                    ready <= '1';
+
                 when READ_REQUEST => 
+
                     -- for reading neuron and synapses
                     -- when last synapse is reached we want to read neuron and synapse. Otherwise we want to read only synapse
+
                     case REQUEST_STATE is
+
                         when READ_NEURON =>
+
                             -- read neuron
                             -- convert neuron counter to 8-bit address
                             neuron_address <= std_logic_vector(to_unsigned(neuron_counter, 8));
+                            MAIN_STATE <= UPDATE;
 
                         when READ_SYNAPSE =>
+
                             -- read synapse
                             if (synapse_counter > 15 and synapse_counter < 32) then
                                 -- multiply neuron counter by 3 and add 1
@@ -88,7 +98,10 @@ begin
                                 synapse_address <= std_logic_vector(to_unsigned(neuron_counter * 3 + 2, 8));
                             end if;
 
+                            MAIN_STATE <= COMPUTE;
+
                         when READ_BOTH =>
+
                             -- read both
                             -- multiply neuron counter by 3
                             synapse_address <= std_logic_vector(to_unsigned(neuron_counter * 3, 8));
@@ -97,25 +110,55 @@ begin
 
                     end case;
 
-                    MAIN_STATE <= COMPUTING;
+                    
 
                 when WRITE_REQUEST =>
                     -- for updating neuron
+                when UPDATE => 
+                    -- update internal signals and variables 
+                    temp_state_core := neuron_input(11 downto 0);
 
-                when COMPUTING => 
+                when COMPUTE => 
                     -- main loop
                     -- start updating the neuron signals from the 32 bit word from neuron bram (neuron_input)
 
                     param_leak_str <= neuron_input(30 downto 24);
                     param_thr <= neuron_input(23 downto 12);
-                    state_core <= neuron_input(11 downto 0);
-
+                    state_core <= temp_state_core;
                     -- we must update the synapse weight. Since we are reading synapses as 32 bit words, we need to extract the weight from the word
                     -- each synapse_in word contains 16 weights. We use the counter signal to index the 2 bit weight
                     syn_weight <= synapse_in(2 * synapse_counter + 1 downto 2 * synapse_counter);
 
                     -- pass the input bit from the input vector. Indexed by the synapse counter
+                    syn_event <= input_vector(synapse_counter - (input_row_counter * 16));
 
+                    -- increment the synapse counter
+                    synapse_counter <= synapse_counter + 1;
+
+                    -- read the state core from the state_core_next signal
+                    temp_state_core := state_core_next;
+
+                    -- check if the neuron has spiked
+                    if spike_out = '1' then
+                        -- if the neuron has spiked we want to do something. Maybe just go to next neuron and store a spike somewhere
+                        synapse_counter <= 48;
+
+                    end if;
+
+                    -- check synapse counter
+                    if synapse_counter = 16 or synapse_counter = 32 then
+                        -- we need to read the following 16 weights and increment the row counter. 
+                        MAIN_STATE <= READ_REQUEST;
+                        REQUEST_STATE <= READ_SYNAPSE;
+                        
+                    elsif synapse_counter = 48 then 
+                        -- next neuron
+                        synapse_counter <= 0;
+                        neuron_counter <= neuron_counter + 1;
+                        MAIN_STATE <= READ_REQUEST;
+                        REQUEST_STATE <= READ_BOTH;
+
+                    end if;
 
                 when others => 
                     MAIN_STATE <= IDLE;
