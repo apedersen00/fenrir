7bc1d304c83e36af66b263839d951dad59767c5a
andreasp <andreas.s.p00@gmail.com>
2025-03-09 11:16:57 +0100

    update top
    Starting with just testing BRAM and lif_neuron.

--- src/design_sources/top.vhd
+++ src/design_sources/top.vhd
@@ -20,7 +20,6 @@ use ieee.numeric_std.all;
 entity snn_top is
     port (
         clk             : in std_logic;
-        data_rdy        : in std_logic;
         input_vector    : in std_logic_vector(15 downto 0);
         spike_out       : out std_logic
     );
@@ -29,45 +28,29 @@ end snn_top;
 architecture Structural of snn_top is
     signal ready           : std_logic;
     signal input_select    : std_logic_vector(3 downto 0);
+
     signal neuron_address  : std_logic_vector(7 downto 0);
     signal neuron_input    : std_logic_vector(31 downto 0);
     signal neuron_we       : std_logic;
     signal neuron_update   : std_logic_vector(31 downto 0);
-    signal synapse_address : std_logic_vector(7 downto 0);
-    signal synapse_in      : std_logic_vector(31 downto 0);
+
+    signal synapse_address  : std_logic_vector(7 downto 0);
+    signal synapse_input    : std_logic_vector(31 downto 0);
+    signal synapse_we       : std_logic;
+    signal synapse_update   : std_logic_vector(31 downto 0);
+
     signal param_leak_str  : std_logic_vector(6 downto 0);
     signal param_thr       : std_logic_vector(11 downto 0);
     signal state_core      : std_logic_vector(11 downto 0);
-    signal syn_weight      : std_logic_vector(1 downto 0);
+    signal syn_weight      : std_logic_vector(3 downto 0);
     signal syn_event       : std_logic;
+    signal time_ref        : std_logic;
     signal state_core_next : std_logic_vector(11 downto 0);
     
 begin
-    -- Instantiate Controller
-    CONTROLLER_INST : entity work.controller
-        port map (
-            clk             => clk,
-            ready           => ready,
-            input_vector    => input_vector,
-            input_select    => input_select,
-            data_rdy        => data_rdy,
-            neuron_address  => neuron_address,
-            neuron_input    => neuron_input,
-            neuron_we       => neuron_we,
-            neuron_update   => neuron_update,
-            synapse_address => synapse_address,
-            synapse_in      => synapse_in,
-            param_leak_str  => param_leak_str,
-            param_thr       => param_thr,
-            state_core      => state_core,
-            syn_weight      => syn_weight,
-            syn_event       => syn_event,
-            state_core_next => state_core_next,
-            spike_out       => spike_out
-        );
 
     -- Instantiate Neuron Memory
-    MEMORY_INST : entity work.rams_init_file
+    MEMORY_INST : entity work.neuron_memory
         port map (
             clk  => clk,
             we   => neuron_we,
@@ -76,6 +59,16 @@ begin
             dout => neuron_input
         );
 
+    -- Instantiate Synapse Memory
+    MEMORY_INST : entity work.synapse_memory
+    port map (
+        clk  => clk,
+        we   => neuron_we,
+        addr => neuron_address,
+        din  => neuron_update,
+        dout => neuron_input
+    );
+
     -- Instantiate LIF Neuron
     LIF_NEURON_INST : entity work.lif_neuron
         port map (
@@ -83,9 +76,9 @@ begin
             param_thr       => param_thr,
             state_core      => state_core,
             state_core_next => state_core_next,
-            syn_weight      => "00" & syn_weight, -- Extend to match 4-bit input
+            syn_weight      => syn_weight,
             syn_event       => syn_event,
-            time_ref        => clk, -- Assuming clk as a timing reference
+            time_ref        => clk,
             spike_out       => spike_out
         );
 
