9287eebb4841dac8d9869b71f23e5fbc8078f1a2
andreasp <andreas.s.p00@gmail.com>
2025-03-09 11:16:53 +0100

    add header to controller

--- src/design_sources/controller.vhd
+++ src/design_sources/controller.vhd
@@ -1,3 +1,18 @@
+/*
+---------------------------------------------------------------------------------------------------
+    Aarhus University (AU, Denmark)
+---------------------------------------------------------------------------------------------------
+
+    File: controller.vhd
+    Description: Controller for 8x6 SNN.
+
+    Author(s):
+        - A. Pedersen, Aarhus University
+        - A. Cherencq, Aarhus University
+
+---------------------------------------------------------------------------------------------------
+*/
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
