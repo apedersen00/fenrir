064dfac10df84613d6e79b6ae16e383909586cd8
andreasp <andreas.s.p00@gmail.com>
2025-03-09 13:06:09 +0100

    add controller to top

--- src/design_sources/top.vhd
+++ src/design_sources/top.vhd
@@ -21,24 +21,49 @@ entity snn_top is
     port (
         clk             : in std_logic;
         input_vector    : in std_logic_vector(15 downto 0);
+        data_rdy        : in std_logic;
         spike_out       : out std_logic
     );
 end snn_top;
 
 architecture Structural of snn_top is
+    -- Controller
     signal ready           : std_logic;
     signal input_select    : std_logic_vector(3 downto 0);
 
+    signal input_vector    : in std_logic_vector(15 downto 0);
+    signal input_select    : out std_logic_vector(3 downto 0);
+    signal data_rdy        : in std_logic;
+    signal neuron_address  : out std_logic_vector(7 downto 0);
+    signal neuron_input    : in std_logic_vector(31 downto 0);
+    signal neuron_we       : out std_logic;
+    signal neuron_update   : out std_logic_vector(31 downto 0);
+
+    signal synapse_address : out std_logic_vector(7 downto 0);
+    signal synapse_in      : in std_logic_vector(31 downto 0);
+
+    signal param_leak_str  : out std_logic_vector(6 downto 0);
+    signal param_thr       : out std_logic_vector(11 downto 0);
+    signal state_core      : out std_logic_vector(11 downto 0);
+    signal syn_weight      : out std_logic_vector(1 downto 0);
+    signal syn_event       : out std_logic;
+    
+    signal state_core_next : in std_logic_vector(11 downto 0);
+    signal spike_out       : in std_logic
+
+    -- Neuron Memory
     signal neuron_address  : std_logic_vector(7 downto 0);
     signal neuron_input    : std_logic_vector(31 downto 0);
     signal neuron_we       : std_logic;
     signal neuron_update   : std_logic_vector(31 downto 0);
 
+    -- Synapse Memory
     signal synapse_address  : std_logic_vector(7 downto 0);
     signal synapse_input    : std_logic_vector(31 downto 0);
     signal synapse_we       : std_logic;
     signal synapse_update   : std_logic_vector(31 downto 0);
 
+    -- LIF Neuron
     signal param_leak_str  : std_logic_vector(6 downto 0);
     signal param_thr       : std_logic_vector(11 downto 0);
     signal state_core      : std_logic_vector(11 downto 0);
@@ -46,7 +71,7 @@ architecture Structural of snn_top is
     signal syn_event       : std_logic;
     signal time_ref        : std_logic;
     signal state_core_next : std_logic_vector(11 downto 0);
-    
+
 begin
 
     -- Instantiate Neuron Memory
@@ -61,13 +86,36 @@ begin
 
     -- Instantiate Synapse Memory
     MEMORY_INST : entity work.synapse_memory
-    port map (
-        clk  => clk,
-        we   => neuron_we,
-        addr => neuron_address,
-        din  => neuron_update,
-        dout => neuron_input
-    );
+        port map (
+            clk  => clk,
+            we   => neuron_we,
+            addr => neuron_address,
+            din  => neuron_update,
+            dout => neuron_input
+        );
+
+    -- Instantiate Controller
+    CONTROLLER_INST : entity work.controller
+        port map (
+            clk             => clk,
+            ready           => ready,
+            input_vector    => input_vector,
+            input_select    => input_select,
+            data_rdy        => data_rdy,
+            neuron_address  => neuron_address,
+            neuron_input    => neuron_input,
+            neuron_we       => neuron_we,
+            neuron_update   => neuron_update,
+            synapse_address => synapse_address,
+            synapse_in      => synapse_input,
+            param_leak_str  => param_leak_str,
+            param_thr       => param_thr,
+            state_core      => state_core,
+            syn_weight      => syn_weight,
+            syn_event       => syn_event,
+            state_core_next => state_core_next,
+            spike_out       => spike_out
+        );
 
     -- Instantiate LIF Neuron
     LIF_NEURON_INST : entity work.lif_neuron
@@ -78,7 +126,7 @@ begin
             state_core_next => state_core_next,
             syn_weight      => syn_weight,
             syn_event       => syn_event,
-            time_ref        => clk,
+            time_ref        => time_ref,
             spike_out       => spike_out
         );
 
