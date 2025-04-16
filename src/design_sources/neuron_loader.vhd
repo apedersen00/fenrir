c1ee8aef9d4f2a1f18f54458bf143758b47a641d
andreasp <andreas.s.p00@gmail.com>
2025-04-16 18:53:05 +0200

    update neuron loader

--- src/design_sources/neuron_loader.vhd
+++ src/design_sources/neuron_loader.vhd
@@ -13,12 +13,15 @@
 ---------------------------------------------------------------------------------------------------
 
     Configuration Registers:
-        - (reg_cfg):
-            - [3:0]     state_width        : width of the state vector
-            - [4:3]     per_neuron_beta_en : enable per neuron beta
-            - [11:4]    common_beta        : beta value when per neuron beta is disabled
-            - [12:11]   per_neuron_thr_en  : enable per neuron threshold
-            - [23:12]   common_thr         : threshold value when per neuron threshold is disabled
+        - (reg_cfg_0):
+            - [3:0]     state_width         : width of the state vector
+            - [4]       per_neuron_beta_en  : enable per neuron beta
+            - [11:4]    common_beta         : beta value when per neuron beta is disabled
+            - [12]      per_neuron_thr_en   : enable per neuron threshold
+            - [23:12]   common_thr          : threshold value when per neuron threshold is disabled
+        - (reg_cfg_1)
+            - [10:0]    layer_size          : number of neurons in the layer
+            - [21:11]   layer_offset        : neuron address layer offset
 
 ---------------------------------------------------------------------------------------------------
 */
@@ -29,16 +32,28 @@ use ieee.numeric_std.all;
 
 entity neuron_loader is
     generic (
-        NUM_REG         : integer -- number of registers in the shotgun
+        SHOTGUN_NUM_REG : integer -- number of registers in the shotgun
     );
     port (
-        -- config
-        i_cfg_en    : in std_logic;                       -- enable configuration
-        i_cfg_addr  : in std_logic_vector(3 downto 0);    -- register to configure
-        i_cfg_val   : in std_logic_vector(31 downto 0);   -- value to configure
+        -- configuration interface
+        i_cfg_en    : in std_logic;                         -- enable configuration
+        i_cfg_addr  : in std_logic_vector(3 downto 0);      -- register to configure
+        i_cfg_val   : in std_logic_vector(31 downto 0);     -- value to configure
 
-        o_reg_addr  : out std_logic_vector(3 downto 0);   -- shotgun register address
-        o_reg_out   : out std_logic_vector(31 downto 0);  -- shotgun register value
+        -- shotgun interface
+        o_reg_addr  : out std_logic_vector(3 downto 0);     -- shotgun register address
+        o_reg_out   : out std_logic_vector(31 downto 0);    -- shotgun register value
+        o_primed    : out std_logic;                        -- shotgun primed signal
+        i_fired     : in std_logic;                         -- shotgun fired signal
+
+        -- neuron memory interface
+        o_nrn_addr  : out std_logic_vector(10 downto 0);    -- neuron address
+        o_nrn_data  : out std_logic_vector(31 downto 0);    -- neuron data
+        o_nrn_en    : out std_logic;                        -- enable neuron memory access
+
+        -- control signals
+        i_start     : in std_logic;                         -- start signal
+        o_busy      : out std_logic;                        -- busy signal
 
         i_clk       : in std_logic;
         i_nrst      : in std_logic;
@@ -48,8 +63,19 @@ entity neuron_loader is
 end neuron_loader;
 
 architecture Behavioral of neuron_loader is
+    -- fsm
+    type state is (
+        IDLE,
+        RUN,
+        WAIT
+    );
+    signal present_state        : state;
+    signal next_state           : state;
+
     -- registers
-    signal reg_cfg              : std_logic_vector(31 downto 0);    -- configuration
+    signal reg_cfg_0            : std_logic_vector(31 downto 0);    -- configuration register 0
+    signal reg_cfg_1            : std_logic_vector(31 downto 0);    -- configuration register 1
+    signal reg_shotgun_counter  : std_logic_vector(3 downto 0);     -- shotgun counter
 
     -- configuration
     signal cfg_state_width      : std_logic_vector(3 downto 0);     -- state width
@@ -57,24 +83,27 @@ architecture Behavioral of neuron_loader is
     signal cfg_common_beta      : std_logic_vector(7 downto 0);     -- common beta value
     signal cfg_per_neuron_thr   : std_logic_vector(1 downto 0);     -- per neuron threshold enable
     signal cfg_common_thr       : std_logic_vector(11 downto 0);    -- common threshold value
+    signal cfg_layer_size       : std_logic_vector(10 downto 0);    -- layer size
+    signal cfg_layer_offset     : std_logic_vector(10 downto 0);    -- layer offset
 
 begin
 
     -- configuration decoding
-    cfg_state_width     <= reg_cfg(3 downto 0);
-    cfg_per_neuron_beta <= reg_cfg(4 downto 3);
-    cfg_common_beta     <= reg_cfg(11 downto 4);
-    cfg_per_neuron_thr  <= reg_cfg(12 downto 11);
-    cfg_common_thr      <= reg_cfg(23 downto 12);
+    cfg_state_width     <= reg_cfg_0(3 downto 0);
+    cfg_per_neuron_beta <= reg_cfg_0(4 downto 3);
+    cfg_common_beta     <= reg_cfg_0(11 downto 4);
+    cfg_per_neuron_thr  <= reg_cfg_0(12 downto 11);
+    cfg_common_thr      <= reg_cfg_0(23 downto 12);
+    cfg_layer_size      <= reg_cfg_1(10 downto 0);
+    cfg_layer_offset    <= reg_cfg_1(21 downto 11);
 
     process(i_clk, i_nrst) is
     begin
         if rising_edge(i_clk) then
-            if i_nrst = '0' then
-                reg_cfg <= (others => '0');
-            elsif i_cfg_en = '1' then
+            if i_cfg_en = '1' then
                 case i_cfg_addr is
-                    when "0000" => reg_cfg <= i_cfg_val;
+                    when "0000" => reg_cfg_0 <= i_cfg_val;
+                    when "0001" => reg_cfg_1 <= i_cfg_val;
                     when others => null;
                 end case;
             end if;
