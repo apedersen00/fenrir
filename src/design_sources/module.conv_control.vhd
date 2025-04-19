33f43b14ab2fb10147f71e72653f178234a538c7
alexthch <201506019@post.au.dk>
2025-04-19 15:09:41 +0200

    add ram conenction

--- src/design_sources/module.conv_control.vhd
+++ src/design_sources/module.conv_control.vhd
@@ -2,10 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Output event format
--- x , y , z
+PACKAGE conv_control_t IS 
 
-package conv_control_t is 
+    CONSTANT IMAGE_WIDTH            :integer:= 10;
+    CONSTANT IMAGE_HEIGHT           :integer:= 10; 
+
+    CONSTANT BITS_PER_NEURON        :integer:= 4;
+    CONSTANT FEATURE_MAPS           :integer:= 2;
+    CONSTANT TIMESTAMP_WIDTH        :integer:= 4;
+    CONSTANT NEURON_ADDRESS_WIDTH   :integer:= 7;
+    CONSTANT MEM_SIZE_OF_ADDRESS    :integer:= BITS_PER_NEURON * FEATURE_MAPS + TIMESTAMP_WIDTH;
+
+    CONSTANT NEURON_RESET_WIDTH      :integer:= 4;
+    CONSTANT NEURON_THRESHOLD_WIDTH  :integer:= 4;
+    CONSTANT KERNEL_BIT_WIDTH        :integer:= 4;
+    CONSTANT LEAKAGE_PARAM_WIDTH     :integer:= 4;
+    CONSTANT TIME_SCALING_FACTOR     :integer:= 200;
 
     type main_states_t is (
         IDLE,
@@ -13,36 +25,71 @@ package conv_control_t is
         UPDATE_ALL_NEURON_TIMESTAMPS
     );
 
-end package conv_control_t;
-package body conv_control_t is end package body conv_control_t;
+    component mem_neuron_potentials
+        port(
+            clka    : in std_logic;
+            clkb    : in std_logic;
+            ena     : in std_logic;
+            enb     : in std_logic;
+            wea     : in std_logic_vector(0 downto 0);
+            web     : in std_logic_vector(0 downto 0);
+            addra   : in std_logic_vector(NEURON_ADDRESS_WIDTH - 1 downto 0);
+            addrb   : in std_logic_vector(NEURON_ADDRESS_WIDTH - 1 downto 0);
+            dina    : in std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0);
+            dinb    : in std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0);
+            douta   : out std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0);
+            doutb   : out std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0)
+        );
+    end component;
+
+END PACKAGE conv_control_t;
+
+package body conv_control_t is 
+end package body conv_control_t;
+
+use work.conv_control_t.all;
 
 entity conv_control is
-    generic(
-        BITS_PER_NEURON : integer := 10;
-        FEATURE_MAPS : integer := 10;
-        TIMESTAMP_WIDTH: integer := 12;
-        NEURON_RESET_WIDTH : integer := 4;
-        NEURON_THRESHOLD_WIDTH : integer := 4;
-        KERNEL_BIT_WIDTH : integer := 4;
-        LEAKAGE_PARAM_WIDTH : integer := 4;
-        TIME_SCALING_FACTOR : integer := 200;
-        IMAGE_WIDTH : integer := 32;
-        IMAGE_HEIGHT : integer := 32;
-        BITS_PER_PIXEL_ADDRESS : integer := 8;
-        KERNEL_SIZE : integer := 3
-    );
     port(
         clk : in std_logic;
-        reset : in std_logic;
-        event_out : out std_logic_vector
+        reset : in std_logic
     );
+end entity conv_control;
 
 architecture fsm of conv_control is
 
     signal state : main_states_t;
+    
+    signal ram_ena : std_logic;
+    signal ram_enb : std_logic;
+    signal ram_wea : std_logic_vector(0 downto 0);
+    signal ram_web : std_logic_vector(0 downto 0);
+    signal ram_addra : std_logic_vector(NEURON_ADDRESS_WIDTH - 1 downto 0);
+    signal ram_addrb : std_logic_vector(NEURON_ADDRESS_WIDTH - 1 downto 0);
+    signal ram_dina : std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0);
+    signal ram_dinb : std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0);
+    signal ram_douta : std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0);
+    signal ram_doutb : std_logic_vector(MEM_SIZE_OF_ADDRESS - 1 downto 0);
+
+    mem_neuron_potentials : mem_neuron_potentials
+    port map(
+        clka => clk,
+        clkb => clk,
+        ena  => ram_ena,
+        enb  => ram_enb,
+        wea  => ram_wea,
+        web  => ram_web,
+        addra => ram_addra,
+        addrb => ram_addrb,
+        dina  => ram_dina,
+        dinb  => ram_dinb,
+        douta => ram_douta,
+        doutb => ram_doutb
+    );
 
 begin
 
 
 
 end architecture fsm;
+ 
