8e8bdba494bb94b664beaf7fc6fe2fbb57aed6ff
andreasp <andreas.s.p00@gmail.com>
2025-04-21 12:38:11 +0200

    update port names in dual-port bram

--- src/design_sources/fifo/dual_port_bram.vhd
+++ src/design_sources/fifo/dual_port_bram.vhd
@@ -25,12 +25,12 @@ entity DUAL_PORT_BRAM is
     );
     port(
         -- write interface
-        i_wen   : in std_logic;                                                     -- write enable
+        i_we    : in std_logic;                                                     -- write enable
         i_waddr : in std_logic_vector(integer(ceil(log2(real(DEPTH))))-1 downto 0); -- write address
         i_wdata : in std_logic_vector(WIDTH - 1 downto 0);                          -- write data
         
         -- read interface
-        i_ren   : in std_logic;                                                     -- read enable
+        i_re    : in std_logic;                                                     -- read enable
         i_raddr : in std_logic_vector(integer(ceil(log2(real(DEPTH))))-1 downto 0); -- read address
         o_rdata : out std_logic_vector(WIDTH - 1 downto 0);                         -- read data
 
@@ -49,7 +49,7 @@ architecture syn of DUAL_PORT_BRAM is
     process(i_clk)
     begin
         if i_clk'event and i_clk = '1' then
-            if i_wen = '1' then
+            if i_we = '1' then
                 RAM(conv_integer(i_waddr)) := i_wdata;
             end if;
         end if;
@@ -58,7 +58,7 @@ architecture syn of DUAL_PORT_BRAM is
     process(i_clk)
     begin
         if i_clk'event and i_clk = '1' then
-            if i_ren = '1' then
+            if i_re = '1' then
                 o_rdata <= RAM(conv_integer(i_raddr));
             end if;
         end if;
