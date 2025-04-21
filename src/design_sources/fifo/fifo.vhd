6091e82bd4deaec52ac4c92196597e41d43b3865
andreasp <andreas.s.p00@gmail.com>
2025-04-21 12:38:13 +0200

    update fifo

--- src/design_sources/fifo/fifo.vhd
+++ src/design_sources/fifo/fifo.vhd
@@ -10,6 +10,9 @@
         - A. Pedersen, Aarhus University
         - A. Cherencq, Aarhus University
 
+    Reference(s):
+        - https://vhdlwhiz.com/ring-buffer-fifo/
+
 ---------------------------------------------------------------------------------------------------
 */
 
@@ -29,6 +32,7 @@ entity BRAM_FIFO is
 
         -- read interface
         i_re            : in std_logic;                                 -- read enable
+        o_rvalid        : out std_logic;                                -- read valid
         o_rdata         : out std_logic_vector(WIDTH - 1 downto 0);     -- read data
 
         -- flags
@@ -37,26 +41,110 @@ entity BRAM_FIFO is
         o_full          : out std_logic;                                -- FIFO full flag
         o_full_next     : out std_logic;                                -- FIFO full next flag
 
+        -- number of elements in FIFO
+        o_fill_count    : out std_logic_vector(integer(ceil(log2(real(DEPTH))))-1 downto 0);
+
         -- auxiliary
         i_clk           : in std_logic;
+        i_rst           : in std_logic;
         o_fault         : out std_logic
     );
 end BRAM_FIFO;
 
 architecture Behavioral of BRAM_FIFO is
 
--- INSERT FIFO HERE:
+    subtype index_t is integer range DEPTH - 1 downto 0;
+    signal head         : index_t;
+    signal tail         : index_t;
 
--- DUAL-PORT BRAM
+    signal empty        : std_logic;
+    signal full         : std_logic;
+    signal fill_count   : integer range DEPTH - 1 downto 0;
 
--- WRITE_POINTER
+    -- BRAM interface signals
+    signal waddr     : std_logic_vector(integer(ceil(log2(real(DEPTH))))-1 downto 0);
+    signal raddr     : std_logic_vector(integer(ceil(log2(real(DEPTH))))-1 downto 0);
 
--- READ_POINTER
-
--- FIFO_LOGIC
+    procedure incr(signal index : inout index_t) is
+    begin
+        if index = index_t'high then
+            index <= index_t'low;
+        else
+            index <= index + 1;
+        end if;
+    end procedure;
 
 begin
 
+    -- copy internal signals to output ports
+    o_empty         <= empty;
+    o_full          <= full;
+    o_fill_count    <= std_logic_vector(to_unsigned(fill_count, o_fill_count'length));
 
+    -- FIFO flags
+    empty           <= '1' when fill_count = 0 else '0';
+    o_empty_next    <= '1' when fill_count <= 1 else '0';
+    full            <= '1' when fill_count >= DEPTH - 1 else '0';
+    o_full_next     <= '1' when fill_count >= DEPTH - 2 else '0';
+
+    fifo_bram: entity work.DUAL_PORT_BRAM
+        generic map (
+            DEPTH => DEPTH,
+            WIDTH => WIDTH
+        )
+        port map (
+            i_we    => i_we,
+            i_waddr => waddr,
+            i_wdata => i_wdata,
+            i_re    => i_re,
+            i_raddr => raddr,
+            o_rdata => o_rdata,
+            i_clk   => i_clk
+        );
+
+    PROC_HEAD : process(i_clk)
+    begin
+        if rising_edge(i_clk) then
+            if i_rst = '1' then
+                head <= 0;
+            else
+                if i_we = '1' and full = '0' then
+                    incr(head);
+                end if;
+            end if;
+        end if;
+    end process;
+
+    PROC_TAIL : process(i_clk)
+    begin
+        if rising_edge(i_clk) then
+            if i_rst = '1' then
+                tail <= 0;
+            else
+                o_rvalid <= '0';
+                if i_re = '1' and empty = '0' then
+                    incr(tail);
+                    o_rvalid <= '1';
+                end if;
+            end if;
+        end if;
+    end process;
+
+    PROC_BRAM : process(i_clk)
+    begin
+        if rising_edge(i_clk) then
+            waddr <= std_logic_vector(to_unsigned(head, waddr'length));
+            raddr <= std_logic_vector(to_unsigned(tail, raddr'length));
+        end if;
+    end process;
+
+    PROC_COUNT : process(head, tail)
+    begin
+        if head < tail then
+            fill_count <= head - tail + DEPTH;
+        else
+            fill_count <= head - tail;
+        end if;
+    end process;
 
 end Behavioral;
