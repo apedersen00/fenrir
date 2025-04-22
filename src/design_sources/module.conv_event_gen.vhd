e4f47f19cbe45a9e3fd9c727f363addbacd5819f
alexthch <201506019@post.au.dk>
2025-04-22 21:10:08 +0200

    shitty vivado compiler

--- src/design_sources/module.conv_event_gen.vhd
+++ src/design_sources/module.conv_event_gen.vhd
@@ -2,8 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
---use work.conv_control_t.all;
-
+package conv_event_gen_pkg is
+    type event_record is record
+        x : integer range 0 to 10 - 1;
+        y : integer range 0 to 10 - 1;
+        timestamp : std_logic_vector(4 - 1 downto 0);
+        events : std_logic_vector(2 - 1 downto 0);
+    end record;
+end package conv_event_gen_pkg;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.conv_event_gen_pkg.all;
 entity conv_event_gen is 
     port(
         clk : in std_logic;
@@ -16,5 +26,42 @@ entity conv_event_gen is
 end entity conv_event_gen;
 
 architecture behavior of conv_event_gen is 
+
+    type event_buffer_arr is array (0 to 10) of event_record;
+    signal event_buffer : event_buffer_arr;
+    signal write_ptr : integer range 0 to 10 := 0;
+    signal read_ptr : integer range 0 to 10 := 0;
+    signal event_count : integer range 0 to 10 := 0;
+    signal event_full : std_logic := '0';
+
 begin
+
+    process(clk, event_happened)
+    begin
+        if rising_edge(clk) then
+            if event_happened = '1' then
+
+                event_buffer(write_ptr).x <= x;
+                event_buffer(write_ptr).y <= y;
+                event_buffer(write_ptr).timestamp <= timestamp;
+                event_buffer(write_ptr).events <= events;
+                write_ptr <= (write_ptr + 1) mod 10;
+                event_count <= event_count + 1;
+
+
+            end if;
+        end if;
+    end process;
+
+    process(clk, event_count)
+    begin
+        if rising_edge(clk) then
+            if event_count > 10 then
+                event_full <= '1';
+            else
+                event_full <= '0';
+            end if;
+        end if;
+    end process;
+
 end architecture behavior;
