0360fccef5fd3ea7e3a23fcef60c2a13225a6685
alexthch <201506019@post.au.dk>
2025-05-03 08:25:27 +0200

    add tb signals

--- src/testbenches/tb_convolution_layer.vhd
+++ src/testbenches/tb_convolution_layer.vhd
@@ -5,3 +5,21 @@ use ieee.numeric_std.all;
 entity tb_convolution_layer is
 end entity tb_convolution_layer;
 
+architecture testbench of tb_convolution_layer is
+
+    CONSTANT CLK_PERIOD : time := 10 ns;
+
+    CONSTANT XCoordinateWidth : integer := 8;
+    CONSTANT YCoordinateWidth : integer := 8;
+    CONSTANT TimeStampWidth : integer := 32;
+
+    signal clk : std_logic := '1';
+    signal reset_o : std_logic := '0';
+    signal config_command_o : std_logic_vector(1 downto 0) := (others => '0');
+    signal config_data_io : std_logic_vector(31 downto 0) := (others => '0');
+    signal event_data_o : std_logic_vector(XCoordinateWidth + YCoordinateWidth + TimeStampWidth - 1 downto 0) := (others => '0');
+    signal event_fifo_empty_no : std_logic := '1';
+    signal event_fifo_read_i : std_logic;
+
+begin
+end architecture testbench;
