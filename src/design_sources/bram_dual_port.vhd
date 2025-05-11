cf7a7ba29bacdf05d3feec93938c71a57a860843
andreasp <andreas.s.p00@gmail.com>
2025-05-11 16:33:49 +0200

    add init file to dual port bram

--- src/design_sources/bram_dual_port.vhd
+++ src/design_sources/bram_dual_port.vhd
@@ -4,6 +4,7 @@
 --
 --  File: dual_port_bram.vhd
 --  Description: Dual-port BRAM with one clock.
+--  VHDL Version: VHDL-2008
 --
 --  Author(s):
 --      - A. Pedersen, Aarhus University
@@ -14,12 +15,32 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 use ieee.math_real.all;
+use std.textio.all;
+
+--  Instantiation Template:
+--  INST_NAME : entity work.DUAL_PORT_BRAM
+--  generic map (
+--      DEPTH       =>
+--      WIDTH       =>
+--      FILENAME    =>
+--  )
+--  port map (
+--      i_we        =>
+--      i_waddr     =>
+--      i_wdata     =>
+--      i_re        =>
+--      i_raddr     =>
+--      o_rdata     =>
+--      i_clk       =>
+--  );
 
 entity DUAL_PORT_BRAM is
     generic(
-        DEPTH : integer;    -- BRAM depth
-        WIDTH : integer     -- BRAM width
+        DEPTH       : integer;      -- BRAM depth
+        WIDTH       : integer;      -- BRAM width
+        FILENAME    : string := ""
     );
     port(
         -- write interface
@@ -40,7 +61,37 @@ end DUAL_PORT_BRAM;
 architecture syn of DUAL_PORT_BRAM is
 
     type ram_type is array (DEPTH - 1 downto 0) of std_logic_vector(WIDTH - 1 downto 0);
-    shared variable RAM : ram_type;
+
+    impure function InitRamFromFile(RamFileName : in string) return ram_type is
+        FILE RamFile         : text;
+        variable file_status : file_open_status;
+        variable RamFileLine : line;
+        variable RAM         : ram_type := (others => (others => '0'));
+        variable v_data      : std_logic_vector(WIDTH - 1 downto 0);
+    begin
+        if RamFileName'length > 0 then
+            file_open(file_status, RamFile, RamFileName, read_mode);
+            
+            if file_status = open_ok then
+                for I in ram_type'range loop
+                    if not endfile(RamFile) then
+                        readline(RamFile, RamFileLine);
+                        read(RamFileLine, v_data);
+                        RAM(I) := v_data;
+                    end if;
+                end loop;
+                file_close(RamFile);
+            else
+                report "Failed to open file: " & RamFileName severity error;
+            end if;
+        else
+            report "No filename provided, initializing RAM with zeros" severity error;
+        end if;
+        
+        return RAM;
+    end function;
+
+    shared variable RAM : ram_type := InitRamFromFile(FILENAME);
 
     begin
 
