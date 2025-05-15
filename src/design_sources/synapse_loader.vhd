f5ea83faf1f7d0b9f4b7b9c52ac1f637544082e3
andreasp <andreas.s.p00@gmail.com>
2025-05-15 09:48:51 +0200

    synapse loader can go to idle from all states

--- src/design_sources/synapse_loader.vhd
+++ src/design_sources/synapse_loader.vhd
@@ -269,13 +269,25 @@ begin
                 end if;
 
             when GET_EVENT =>
-                next_state  <= GET_WEIGHTS;
+                if (i_goto_idle = '1') then
+                    next_state <= IDLE;
+                else
+                    next_state  <= GET_WEIGHTS;
+                end if;
             
             when GET_WEIGHTS =>
-                next_state  <= WAIT_FOR_BRAM;
+                if (i_goto_idle = '1') then
+                    next_state <= IDLE;
+                else
+                    next_state  <= WAIT_FOR_BRAM;
+                end if;
 
             when WAIT_FOR_BRAM =>
-                next_state <= ITERATE;
+                if (i_goto_idle = '1') then
+                    next_state <= IDLE;
+                else
+                    next_state <= ITERATE;
+                end if;
 
             when ITERATE =>
                 if (i_goto_idle = '1') then
