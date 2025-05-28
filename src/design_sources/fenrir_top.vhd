20bdb7fdc89a81dd2a166ae3ee40417a9dc337e2
andreasp <andreas.s.p00@gmail.com>
2025-05-28 22:20:22 +0200

    is this the most retarded way to write a fifo?

--- src/design_sources/fenrir_top.vhd
+++ src/design_sources/fenrir_top.vhd
@@ -27,28 +27,13 @@ entity FENRIR_TOP is
         spike_1 : out std_logic_vector(31 downto 0);
         spike_2 : out std_logic_vector(31 downto 0);
 
-        -- BRAM 1 interface PS
-        bram_addr_a    : in std_logic_vector(11 downto 0);
-        bram_clk_a     : in std_logic;
-        bram_wrdata_a  : in std_logic_vector(32 - 1 downto 0);
-        bram_rddata_a  : out std_logic_vector(32 - 1 downto 0);
-        bram_en_a      : in std_logic;
-        bram_rst_a     : in std_logic;
-        bram_we_a      : in std_logic_vector((32 / 8) - 1 downto 0)
+        -- PS FIFO
+        ps_fifo : in std_logic_vector(31 downto 0)
     );
 
 end FENRIR_TOP;
 
 architecture behavior of FENRIR_TOP is
-
-    ATTRIBUTE X_INTERFACE_INFO : string;
-    ATTRIBUTE X_INTERFACE_INFO OF bram_addr_a    : SIGNAL IS "xilinx.com:interface:bram:1.0 BRAM_PORTA ADDR";
-    ATTRIBUTE X_INTERFACE_INFO OF bram_clk_a     : SIGNAL IS "xilinx.com:interface:bram:1.0 BRAM_PORTA CLK";
-    ATTRIBUTE X_INTERFACE_INFO OF bram_wrdata_a  : SIGNAL IS "xilinx.com:interface:bram:1.0 BRAM_PORTA DIN";
-    ATTRIBUTE X_INTERFACE_INFO OF bram_rddata_a  : SIGNAL IS "xilinx.com:interface:bram:1.0 BRAM_PORTA DOUT";
-    ATTRIBUTE X_INTERFACE_INFO OF bram_en_a      : SIGNAL IS "xilinx.com:interface:bram:1.0 BRAM_PORTA EN";
-    ATTRIBUTE X_INTERFACE_INFO OF bram_rst_a     : SIGNAL IS "xilinx.com:interface:bram:1.0 BRAM_PORTA RST";
-    ATTRIBUTE X_INTERFACE_INFO OF bram_we_a      : SIGNAL IS "xilinx.com:interface:bram:1.0 BRAM_PORTA WE";
 
     -- fsm
     type state is (
@@ -65,103 +50,65 @@ architecture behavior of FENRIR_TOP is
     signal spike_2_reg  : unsigned(31 downto 0);
     signal clk_div      : unsigned(1 downto 0);
 
-    signal testmem_we       : std_logic;
-    signal testmem_waddr    : std_logic_vector(7 downto 0);
-    signal testmem_wdata    : std_logic_vector(31 downto 0);
-    signal testmem_re       : std_logic;
-    signal testmem_raddr    : std_logic_vector(7 downto 0);
-    signal testmem_rdata    : std_logic_vector(31 downto 0);
-    signal testmem_clk      : std_logic;
+    signal fifo_we      : std_logic;
+    signal fifo_wdata   : std_logic_vector(15 downto 0);
+    signal fifo_re      : std_logic;
+    signal fifo_rdata   : std_logic_vector(15 downto 0);
+    signal fifo_empty   : std_logic;
+    signal fifo_rvalid  : std_logic;
 
-    signal synmem_we        : std_logic;
-    signal synmem_waddr     : std_logic_vector(7 downto 0);
-    signal synmem_wdata     : std_logic_vector(31 downto 0);
-    signal synmem_re        : std_logic;
-    signal synmem_raddr     : std_logic_vector(7 downto 0);
-    signal synmem_rdata     : std_logic_vector(31 downto 0);
-
-    signal bram_sel         : std_logic;
+    signal reset        : std_logic;
 
 begin
 
-    bram_sel <= '0' when present_state = CONFIG else '1';
-    synmem_re       <=  '1';
-    synmem_raddr    <=  std_logic_vector(to_unsigned(1, synmem_raddr'length));
-
-    TEST_MEMORY : entity work.DUAL_PORT_BRAM
+    TEST_ADAPTER : entity work.FIFO_ADAPTER
     generic map (
-        DEPTH       => 256,
-        WIDTH       => 32,
-        FILENAME    => ""
+        WIDTH           => 16
     )
     port map (
-        i_we        => testmem_we,
-        i_waddr     => testmem_waddr,
-        i_wdata     => testmem_wdata,
-        i_re        => testmem_re,
-        i_raddr     => testmem_raddr,
-        o_rdata     => testmem_rdata,
-        i_clk       => testmem_clk
+        i_clk           => sysclk,
+        i_rst           => reset,
+        i_ps_write      => ps_fifo,
+        o_fifo_we       => fifo_we,
+        o_fifo_wdata    => fifo_wdata
     );
 
-    BRAM_MUX_0 : entity work.BRAM_MUX
+    TEST_FIFO : entity work.BRAM_FIFO
     generic map (
-        RAM_WIDTH   => 32,
-        RAM_DEPTH   => 256,
-        PS_DEPTH    => 2048
+        DEPTH   => 256,
+        WIDTH   => 16
     )
     port map (
-        i_clk               => sysclk,
-        i_sel               => bram_sel,
-        -- BRAM interface
-        o_we                => testmem_we,
-        o_waddr             => testmem_waddr,
-        o_wdata             => testmem_wdata,
-        o_re                => testmem_re,
-        o_raddr             => testmem_raddr,
-        i_rdata             => testmem_rdata,
-        o_clk               => testmem_clk,
-        -- BRAM port A interface PS
-        i_ps_bram_addr_a    => bram_addr_a,
-        i_ps_bram_clk_a     => bram_clk_a,
-        i_ps_bram_wrdata_a  => bram_wrdata_a,
-        o_ps_bram_rddata_a  => bram_rddata_a,
-        i_ps_bram_en_a      => bram_en_a,
-        i_ps_bram_rst_a     => bram_rst_a,
-        i_ps_bram_we_a      => bram_we_a,
-        -- BRAM interface PL
-        i_pl_we             => synmem_we, 
-        i_pl_waddr          => synmem_waddr,
-        i_pl_wdata          => synmem_wdata,
-        i_pl_re             => synmem_re,
-        i_pl_raddr          => synmem_raddr,
-        o_pl_rdata          => synmem_rdata,
-        i_pl_clk            => sysclk
+        i_we            => fifo_we,
+        i_wdata         => fifo_wdata,
+        i_re            => fifo_re,
+        o_rvalid        => fifo_rvalid,
+        o_rdata         => fifo_rdata,
+        o_empty         => fifo_empty,
+        o_empty_next    => open,
+        o_full          => open,
+        o_full_next     => open,
+        o_fill_count    => open,
+        i_clk           => sysclk,
+        i_rst           => reset
     );
 
-    -- READ_BRAM : process(sysclk)
-    -- begin
-    --     if rising_edge(sysclk) then
-    --         if present_state = RUN then
-    --             synmem_we       <= '0';
-    --             synmem_waddr    <=  (others => '0');
-    --             synmem_wdata    <=  (others => '0');
-    --             synmem_re       <=  '1';
-    --             synmem_raddr    <=  std_logic_vector(to_unsigned(1, synmem_raddr'length));
-    --         else
-    --             synmem_we       <= '0';
-    --             synmem_waddr    <=  (others => '0');
-    --             synmem_wdata    <=  (others => '0');
-    --             synmem_re       <=  '0';
-    --             synmem_raddr    <=  (others => '0');
-    --         end if;
-    --     end if;
-    -- end process;
+    READ_FIFO : process(sysclk)
+    begin
+        if rising_edge(sysclk) then
+            if (fifo_empty = '0') and (fifo_re = '0') then
+                fifo_re         <= '1';
+            elsif (fifo_re = '1') and (fifo_rvalid = '1') then
+                spike_2         <= "0000000000000000" & fifo_rdata;
+                fifo_re         <= '0';
+            end if;
+        end if;
+    end process;
 
     -- Debugging
     spike_0 <= std_logic_vector(spike_0_reg);
     spike_1 <= std_logic_vector(spike_1_reg);
-    spike_2 <= testmem_rdata;
+    -- spike_2 <= std_logic_vector(spike_2_reg);
 
     increment : process(sysclk)
     begin
@@ -182,6 +129,17 @@ begin
                 if clk_div = "00" then
                     spike_2_reg <= spike_2_reg + 1;
                 end if;
+            end if;
+        end if;
+    end process;
+
+    reset_proc : process(sysclk)
+    begin
+        if rising_edge(sysclk) then
+            if present_state = IDLE then
+                reset <= '1';
+            else
+                reset <= '0';
             end if;
         end if;
     end process;
@@ -221,7 +179,11 @@ begin
             when CONFIG =>
                 led <= "0011";
             when RUN =>
-                led <= "0111";
+                if fifo_empty = '1' then
+                    led <= "0111";
+                else
+                    led <= "1111";
+                end if;
             when INVALID =>
                 led <= "0000";
         end case;
