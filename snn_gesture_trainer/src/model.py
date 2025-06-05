5434384302af3f2be313609e2221be261f6cfef0
andreasp <andreas.s.p00@gmail.com>
2025-06-05 18:12:36 +0200

    add FC only model
    squash me

--- snn_gesture_trainer/src/model.py
+++ snn_gesture_trainer/src/model.py
@@ -79,3 +79,59 @@ class FenrirNet(nn.Module):
             spk_rec.append(spk4)
 
         return torch.stack(spk_rec)
+
+class FenrirFC(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        num_classes     = int(config["num_classes"])
+        w               = int(config["target_width"])
+        h               = int(config["target_height"])
+        fc1_bits        = int(config["fc1_bits"])
+        fc2_bits        = int(config["fc2_bits"])
+        fc1_beta_init   = config["fc1_beta"]
+        fc2_beta_init   = config["fc2_beta"]
+        fc1_thr_init    = config["fc1_thr"]
+        fc2_thr_init    = config["fc2_thr"]
+        self.fc1_multiplier  = config["fc1_multiplier"]
+        self.fc2_multiplier  = config["fc2_multiplier"]
+
+        self.fc1_beta   = torch.nn.Parameter(torch.tensor(fc1_beta_init), requires_grad=True)
+        self.fc1        = qnn.QuantLinear(60*60, 64, bias=False, weight_bit_width=fc1_bits)
+        self.lif1       = snn.Leaky(beta=1.0, threshold=fc1_thr_init, learn_threshold=True, reset_mechanism='zero', reset_delay=False)
+
+        self.fc2_beta   = torch.nn.Parameter(torch.tensor(fc2_beta_init), requires_grad=True)
+        self.fc2        = qnn.QuantLinear(64, num_classes, bias=False, weight_bit_width=fc2_bits)
+        self.lif2       = snn.Leaky(beta=1.0, threshold=fc2_thr_init, learn_threshold=True, reset_mechanism='zero', reset_delay=False)
+
+    def forward(self, x: torch.Tensor):
+
+        B, T, C, H, W = x.shape
+
+        fc_mem1   = self.lif1.init_leaky()
+        fc_mem2   = self.lif2.init_leaky()
+
+        # Record output spikes
+        spk_rec = []
+
+        scale_fc1 = self.fc1.quant_weight().scale
+        scale_fc2 = self.fc2.quant_weight().scale
+
+        for t in range(T):
+
+            xt = x[:, t, :, :, :]
+            xt = xt.contiguous().view(B, -1)
+
+            cur1 = self.fc1(xt)
+            fc_mem1 = NetUtils.mem_clamp(fc_mem1, scale_fc1, multiplier=self.fc1_multiplier)
+            spk1, fc_mem1 = self.lif1(cur1, fc_mem1)
+            fc_mem1 = NetUtils.beta_clamp(fc_mem1, self.fc1_beta)
+
+            cur2 = self.fc2(spk1)
+            fc_mem2 = NetUtils.mem_clamp(fc_mem2, scale_fc2, multiplier=self.fc2_multiplier)
+            spk2, fc_mem2 = self.lif2(cur2, fc_mem2)
+            fc_mem2 = NetUtils.beta_clamp(fc_mem2, self.fc2_beta)
+
+            spk_rec.append(spk2)
+
+        return torch.stack(spk_rec)
