be5da4ab13b46134e40fee7ea7850c42cc3b5445
alexthch <201506019@post.au.dk>
2025-06-09 11:53:52 +0200

    update interface for k ram

--- src/design_sources/convolution/interfaces/kernel_bram_if.sv
+++ src/design_sources/convolution/interfaces/kernel_bram_if.sv
@@ -6,7 +6,7 @@ interface kernel_bram_if #(
     
     parameter int DATA_WIDTH = KERNEL_WEIGHT_BITS * OUT_CHANNELS,
     // figure out how many kernel weights total
-    parameter int TOTAL_KERNEL_POSITIONS = IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE
+    parameter int TOTAL_KERNEL_POSITIONS = IN_CHANNELS * KERNEL_SIZE * KERNEL_SIZE,
     parameter int ADDR_WIDTH = $clog2(TOTAL_KERNEL_POSITIONS)
 );
     // Single port BRAM interface for kernel weights
@@ -14,4 +14,26 @@ interface kernel_bram_if #(
     logic clk;
     logic rst_n;
 
-    logic []
+    logic [ADDR_WIDTH-1:0] addr;      // Address for kernel weights
+    logic [DATA_WIDTH-1:0] data_in;   // Data input for kernel weights
+    logic [DATA_WIDTH-1:0] data_out;  // Data output from kernel weights
+    logic                  we;        // Write enable for kernel weights
+    logic                  en;        // Enable signal for kernel weights
+
+    modport bram_module(
+        input clk,
+        input rst_n,
+        input addr,
+        input data_in,
+        output data_out,
+        input we,
+        input en
+    );
+
+    modport conv_module(
+        output en,
+        output addr,
+        input data_out
+    );
+
+endinterface
