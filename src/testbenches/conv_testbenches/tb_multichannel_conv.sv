ab1037e41844fce0d66cf2bb28cf6e658bf56028
alexthch <201506019@post.au.dk>
2025-06-09 13:46:38 +0200

    tb params

---
+++ src/testbenches/conv_testbenches/tb_multichannel_conv.sv
@@ -0,0 +1,14 @@
+import snn_interfaces_pkg::*;
+
+module tb_conv_multichannel;
+
+    localparam int COORD_BITS = 3; // address space 0-7
+    localparam int IN_CHANNELS = 2;
+    localparam int OUT_CHANNELS = 2;
+    localparam int KERNEL_SIZE = 3;
+    localparam int IMG_WIDTH = 8;
+    localparam int IMG_HEIGHT = 8;
+
+    
+
+endmodule
