0861fb72ce059d95cccb7494b7250a2f48b5ac54
alexthch <201506019@post.au.dk>
2025-06-10 19:26:49 +0200

    refactor pkg

---
+++ src/design_sources/convolution/packages/conv_pkg.sv
@@ -0,0 +1,15 @@
+package conv_pkg;
+
+    // CONVOLUTION PARAMETERS
+    parameter int DEFAULT_KERNEL_SIZE = 3;
+    parameter int DEFAULT_IN_CHANNELS = 2;
+    parameter int DEFAULT_OUT_CHANNELS = 2;
+    parameter int DEFAULT_IMG_HEIGHT = 8;
+    parameter int DEFAULT_IMG_WIDTH = 8;
+
+    // MEMORY PARMETERS 
+    parameter int DEFAULT_BITS_PER_KERNEL_WEIGHT = 6;
+    parameter int DEFAULT_BITS_PER_COORDINATE = 4;
+    
+
+endpackage
