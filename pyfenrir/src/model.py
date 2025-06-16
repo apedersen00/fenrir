b98d8ae6d879b01cd06bc804fb3b3867c7cd0185
andreasp <andreas.s.p00@gmail.com>
2025-06-16 15:27:38 +0200

    add batch normalization and dropout

--- pyfenrir/src/model.py
+++ pyfenrir/src/model.py
@@ -9,26 +9,31 @@ class FenrirNet(nn.Module):
     def __init__(self, config):
         super().__init__()
 
-        num_classes     = int(config["num_classes"])
-        w               = int(config["target_width"])
-        h               = int(config["target_height"])
-        conv1_out       = int(config["conv1_out"])
-        conv2_out       = int(config["conv2_out"])
-        conv3_out       = int(config["conv3_out"])
-        conv4_out       = int(config["conv4_out"])
-        kernel_size     = int(config["kernel_size"])
-        fc1_bits        = int(config["fc1_bits"])
-        fc2_bits        = int(config["fc2_bits"])
-        fc1_beta_init   = config["fc1_beta"]
-        fc2_beta_init   = config["fc2_beta"]
-        fc1_thr_init    = config["fc1_thr"]
-        fc2_thr_init    = config["fc2_thr"]
-        self.fc1_multiplier  = config["fc1_multiplier"]
-        self.fc2_multiplier  = config["fc2_multiplier"]
+        num_classes         = int(config["num_classes"])
+        w                   = int(config["target_width"])
+        h                   = int(config["target_height"])
+        conv1_out           = int(config["conv1_out"])
+        conv2_out           = int(config["conv2_out"])
+        conv3_out           = int(config["conv3_out"])
+        conv4_out           = int(config["conv4_out"])
+        kernel_size         = int(config["kernel_size"])
+        fc1_bits            = int(config["fc1_bits"])
+        fc2_bits            = int(config["fc2_bits"])
+        fc1_beta_init       = config["fc1_beta"]
+        fc2_beta_init       = config["fc2_beta"]
+        fc1_thr_init        = config["fc1_thr"]
+        fc2_thr_init        = config["fc2_thr"]
+        self.fc1_multiplier = config["fc1_multiplier"]
+        self.fc2_multiplier = config["fc2_multiplier"]
+        self.batch_norm     = config["batch_norm"]
+        self.dropout_p1     = config["dropout"]
 
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=conv1_out, kernel_size=kernel_size, stride=1, padding=1, padding_mode='zeros', bias=False)
+        self.bn1   = nn.BatchNorm2d(conv1_out)
         self.conv2 = nn.Conv2d(in_channels=conv1_out, out_channels=conv2_out, kernel_size=kernel_size, stride=1, padding=1, padding_mode='zeros', bias=False)
+        self.bn2   = nn.BatchNorm2d(conv2_out)
         self.conv3 = nn.Conv2d(in_channels=conv2_out, out_channels=conv3_out, kernel_size=kernel_size, stride=1, padding=1, padding_mode='zeros', bias=False)
+        self.bn3   = nn.BatchNorm2d(conv3_out)
 
         nn.init.kaiming_normal_(self.conv1.weight)
         nn.init.kaiming_normal_(self.conv2.weight)
@@ -45,6 +50,8 @@ class FenrirNet(nn.Module):
         self.fc2_beta   = torch.nn.Parameter(torch.tensor(fc2_beta_init), requires_grad=True)
         self.fc2        = qnn.QuantLinear(256, num_classes, bias=False, weight_bit_width=fc2_bits)
         self.lif2       = snn.Leaky(beta=1.0, threshold=fc2_thr_init, learn_threshold=True, reset_mechanism='zero', reset_delay=False)
+
+        self.dropout    = nn.Dropout(self.dropout_p1)
 
     def forward(self, x: torch.Tensor):
 
@@ -67,15 +74,22 @@ class FenrirNet(nn.Module):
             xt = x[:, t, :, :, :]
 
             conv1_out = self.conv1(xt)
+            if self.batch_norm:
+                conv1_out = self.bn1(conv1_out)
             conv_mem1, spk1 = self.pool1(conv_mem1, conv1_out)
 
             conv2_out = self.conv2(spk1)
+            if self.batch_norm:
+                conv2_out = self.bn2(conv2_out)
             conv_mem2, spk2 = self.pool2(conv_mem2, conv2_out)
 
             conv3_out = self.conv3(spk2)
+            if self.batch_norm:
+                conv3_out = self.bn3(conv3_out)
             conv_mem3, spk3 = self.pool3(conv_mem3, conv3_out)
 
             cur1 = self.fc1(spk3.view(B, -1))
+            cur1 = self.dropout(cur1)
             fc_mem1 = NetUtils.mem_clamp(fc_mem1, scale_fc1, multiplier=self.fc1_multiplier)
             spk4, fc_mem1 = self.lif1(cur1, fc_mem1)
             fc_mem1 = NetUtils.beta_clamp(fc_mem1, self.fc1_beta)
